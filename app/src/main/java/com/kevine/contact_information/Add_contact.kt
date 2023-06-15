225c6c7c9d1e20655e010396ddda18b64ec3a0db
student <kevinenzayanga@gmail.com>
2023-06-15 13:24:29 +0300

    changed the add contact file better

--- app/src/main/java/com/kevine/contact_information/Add_contact.kt
+++ app/src/main/java/com/kevine/contact_information/Add_contact.kt
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.kevine.contact_information.databinding.ActivityAddContactBinding
-import com.kevine.contact_information.databinding.ActivityMainBinding
 
 class Add_contact : AppCompatActivity() {
     lateinit var binding: ActivityAddContactBinding
@@ -15,23 +16,86 @@ class Add_contact : AppCompatActivity() {
         binding= ActivityAddContactBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        clearErrorsOnType()
+        val button2 = findViewById<Button>(R.id.btsave)
+        button2.setOnClickListener {
+            val intent = Intent(this@Add_contact, MainActivity::class.java)
+            startActivity(intent)
+        }
+        onResume()
     }
 
-    fun clearErrorsOnType() {
-        binding.tinameentry.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    override fun onResume() {
+        super.onResume()
+        clearErrorOnType()
+        validateRegisterContacts()
+    }
+    fun clearErrorOnType(){
+        binding.tinameentry.addTextChangedListener ( object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                binding.tinameentry.error=null
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.tlname.error = null
             }
 
-            override fun afterTextChanged(p0: Editable?) {
+            override fun afterTextChanged(s: Editable?) {
+
             }
-        })
+        } )
+        binding.tinumber.addTextChangedListener ( object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.tlnum.error = null
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        } )
+        binding.tiemail.addTextChangedListener ( object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.tlemail.error = null
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        } )
+    }
+    fun validateRegisterContacts(){
+        val name =binding. tinameentry.text.toString()
+        val contacts = binding.tinumber.text.toString()
+        val email =binding. tiemail.text.toString()
+
+        var error = false
+        if (name.isBlank()){
+            binding. tlname.error = "name is required"
+            error = true
+        }
+
+        if (contacts.isBlank()){
+            binding.tinumber.error = "contacts is required"
+            error = true
+        }
+
+        if (email.isBlank()){
+            binding.tiemail.error = "Email is required"
+            error = true
+        }
+
+
+        if (!error){
+            Toast.makeText(this,"$name,$contacts, $email", Toast.LENGTH_LONG).show()
+        }
 
     }
-
 
 }
